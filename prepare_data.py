452c4c6571508fffe27812ba09063c1923f3c784
Francesco <33670163+Fenerator@users.noreply.github.com>
2023-04-21 15:36:17 +0200

    prints

--- prepare_data.py
+++ prepare_data.py
@@ -6,12 +6,13 @@ def prepare_data(data_path: str, batch_size: int) -> tuple:
     """_summary_
 
     Args:
-        data_path (str): _description_
+        data_path (str):
         batch_size (int):
 
     Returns:
         tuple: 3 dataloaders and vocab
     """
+    data_path = Path(data_path)
 
     # define preprocessing pipeline
     tokenizer = torchtext.data.utils.get_tokenizer("spacy", language="en_core_web_sm")
@@ -19,9 +20,11 @@ def prepare_data(data_path: str, batch_size: int) -> tuple:
     LABEL = torchtext.data.Field(sequential=False, unk_token=None)  # unk_token=None to prevent unk token in labels
 
     # get data
+    print("Preparing SNLI data...")
     train, val, test = torchtext.datasets.SNLI.splits(text_field=TEXT, label_field=LABEL, root=data_path)
 
     # build vocabulary
+    print("Building vocabulary...")
     TEXT.build_vocab(train, vectors=torchtext.vocab.GloVe(cache=data_path / "GloVe/"))
     LABEL.build_vocab(train)
 
