56d8c3232478345aa1213888f26c29f4e1fbd342
Francesco <33670163+Fenerator@users.noreply.github.com>
2023-05-17 18:51:49 +0200

    fix encoding issue

--- brain/align2text.py
+++ brain/align2text.py
@@ -1,18 +1,20 @@
-import tgt
+import textgrid
 import sys
 from funcs import text2fmri
 import pickle
 
-def main(argv) :
+
+def main(argv):
     PATH = argv[1]
     SENT_N = argv[2]
-    for lan in ['EN', 'FR', 'CN'] :
+    for lan in ["EN", "FR", "CN"]:
         aligns = []
-        for i in range(1,10) :
-            tg = tgt.io.read_textgrid(f"{PATH}/annotation/{lan}/lpp{lan}_section{i}.TextGrid")
+        for i in range(1, 10):
+            tg = textgrid.TextGrid.fromFILE(f"{PATH}/annotation/{lan}/lpp{lan}_section{i}.TextGrid")
             aligns.append(text2fmri(tg, SENT_N))
-        with open(f"data/{lan}_aligned_words.pickle", 'wb') as p :
+        with open(f"data/{lan}_aligned_words.pickle", "wb") as p:
             pickle.dump(aligns, p)
 
-if __name__ == "__main__" : 
+
+if __name__ == "__main__":
     main(sys.argv)
