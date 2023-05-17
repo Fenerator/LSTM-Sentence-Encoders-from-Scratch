bd5bcbb02c6b2e312be2910dd1ff557174ac962d
Francesco <33670163+Fenerator@users.noreply.github.com>
2023-05-17 22:33:18 +0200

    error handling

--- brain/sent_repr.py
+++ brain/sent_repr.py
@@ -13,19 +13,26 @@ model = model.to(device)
 
 PATH = "/project/gpuuva021/shared/FMRI-Data"
 
-for lang in ["EN", "CN", "FR"]:
+# for lang in ["EN", "FR", "CN"]:
+for lang in ["FR"]:
     print(f"Encoding Language: {lang}")
 
     # read the aligned words
     with open(f"{PATH}/text_data/{lang}_aligned_words.pickle", "rb") as f:
-        words = pickle.load(f)  # each section contains list of scans; list of words
+        aligned_words = pickle.load(f)  # each section contains list of scans; list of words
 
     # store encodings of each chunk
     hidden_states = []
-    for i, sec in enumerate(words):
-        encoded_input = tokenizer(sec, return_tensors="pt", padding=True).to(device)
-
+    for i, section in enumerate(aligned_words):
         print(f"Encoding Sec {i}")
+        
+        assert len(section) > 0, f"Section {i} has 0 chunks \n {section}"
+        
+        # encode the section
+        try:
+            encoded_input = tokenizer(section, return_tensors="pt", padding=True).to(device)
+        except IndexError:
+            print(f"=== IndexError: section {i} \n {section} ===")
 
         output = model(**encoded_input, output_hidden_states=True)
 
