f555c0abb37d7b0e73330a808316e20e0cdc7707
Francesco <33670163+Fenerator@users.noreply.github.com>
2023-05-17 22:01:35 +0200

    paths

--- brain/sent_repr.py
+++ brain/sent_repr.py
@@ -4,7 +4,6 @@ import pickle
 import numpy as np
 
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
-device = 'cpu'
 
 print(f"Using device: {device}")
 
@@ -12,12 +11,13 @@ tokenizer = AutoTokenizer.from_pretrained("xlm-roberta-base")
 model = AutoModelForMaskedLM.from_pretrained("xlm-roberta-base")
 model = model.to(device)
 
+PATH = "/project/gpuuva021/shared/FMRI-Data"
 
 for lang in ["EN", "CN", "FR"]:
     print(f"Encoding Language: {lang}")
 
     # read the aligned words
-    with open(f"./text_data/{lang}_aligned_words.pickle", "rb") as f:
+    with open(f"{PATH}/text_data/{lang}_aligned_words.pickle", "rb") as f:
         words = pickle.load(f)  # each section contains list of scans; list of words
 
     # store encodings of each chunk
@@ -36,5 +36,5 @@ for lang in ["EN", "CN", "FR"]:
         hidden_states.append(list(h_s))
 
     # save the hidden states in a list of tensors (shape: batch_size, sequence_length, hidden_size)
-    with open(f"/project/gpuuva021/shared/FMRI-Data/aligned/{lang}_hidden_states.pickle", "wb") as cache_file:
-        pickle.dump(hidden_states, cache_file)
+    with open(f"{PATH}/aligned/{lang}_hidden_states.pickle", "wb") as f:
+        pickle.dump(hidden_states, f)
