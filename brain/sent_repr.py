43b24f4faf3cc7cbd8a22f2ca02df8f0096bee04
Francesco <33670163+Fenerator@users.noreply.github.com>
2023-05-18 09:20:43 +0200

    prints

--- brain/sent_repr.py
+++ brain/sent_repr.py
@@ -13,8 +13,7 @@ model = model.to(device)
 
 PATH = "/project/gpuuva021/shared/FMRI-Data"
 
-# for lang in ["EN", "FR", "CN"]:
-for lang in ["FR"]:
+for lang in ["EN", "FR", "CN"]:
     print(f"Encoding Language: {lang}")
 
     # read the aligned words
@@ -24,24 +23,24 @@ for lang in ["FR"]:
     # store encodings of each chunk
     hidden_states = []
     for i, section in enumerate(aligned_words):
-        print(f"Encoding Sec {i}")
+        print(f"Encoding Sec {i}, len(section): {len(section)}")
         
-        assert len(section) > 0, f"Section {i} has 0 chunks \n {section}"
+        #assert len(section) > 0, f"Section {i} has 0 chunks \n {section}"
         
         # encode the section
-        try:
-            encoded_input = tokenizer(section, return_tensors="pt", padding=True).to(device)
-        except IndexError:
-            print(f"=== IndexError: section {i} \n {section} ===")
+    #     try:
+    #         encoded_input = tokenizer(section, return_tensors="pt", padding=True).to(device)
+    #     except IndexError:
+    #         print(f"=== IndexError: section {i} \n {section} ===")
 
-        output = model(**encoded_input, output_hidden_states=True)
+    #     output = model(**encoded_input, output_hidden_states=True)
 
-        h_s = output.hidden_states
+    #     h_s = output.hidden_states
 
-        print(f"last hidden state shape: {h_s[-1].shape}")
+    #     print(f"last hidden state shape: {h_s[-1].shape}")
 
-        hidden_states.append(list(h_s))
+    #     hidden_states.append(list(h_s))
 
-    # save the hidden states in a list of tensors (shape: batch_size, sequence_length, hidden_size)
-    with open(f"{PATH}/aligned/{lang}_hidden_states.pickle", "wb") as f:
-        pickle.dump(hidden_states, f)
+    # # save the hidden states in a list of tensors (shape: batch_size, sequence_length, hidden_size)
+    # with open(f"{PATH}/aligned/{lang}_hidden_states.pickle", "wb") as f:
+    #     pickle.dump(hidden_states, f)
